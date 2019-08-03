8e0af46f5eb87ab5794666af54ebd150a885bed5
Rupali Singh <rupali26d@gmail.com>
2019-08-04 01:17:50 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first';
+  task: String = ""
+  visible=true
+  names: Array<any> = [
+    {name: "learn angular", completed: false},
+    {name: "make a project on angular", completed: false},
+    {name: "host it on github", completed: false},
+    {name: "host the project live", completed: false}
+  ]
+  click = ()=>{
+   // console.log("hi "+ str)
+   let ob = {name: this.task, completed: false}
+    this.names.push(ob)
+    this.task = ''
+  }
+  mark(i){
+    this.names[i].completed = !this.names[i].completed;
+  }
+  delete=(i)=>{
+    console.log('delete '+ i)
+    this.names.splice(i, 1)
+
+  }
+  constructor(){
+    
+
+  }
+  ngOnInit(){
+
+  }
 }
